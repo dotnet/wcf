b7585dbebec131250ca95a2c34a794067d8ad893
iamjasonp <jasonpa@microsoft.com>
2015-05-19 17:57:36 +0800

    Responding to PR comments
    * Split BasicBindingTests.cs into BasicHttp, Custom, and NetHttp binding test files

---
+++ src/System.Private.ServiceModel/tests/Scenarios/Http/TransportChannel/BasicBindings/CustomBindingWithHttpTransportTests.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TestTypes;
+using Xunit;
+
+public static class Http_TransportChannel_BasicBindings_CustomBindingWithHttpTransportTests
+{
+    // Client and Server bindings setup exactly the same using default settings.
+    [Fact]
+    [OuterLoop]
+    public static void SameBinding_Https_Soap12_EchoString_RoundTrips()
+    {
+        string testCaseName = "HttpsTransportBindingElement_ScenarioTests.SameBinding_DefaultSettings_EchoString";
+        string variationDetails = "Client:: CustomBinding/DefaultValues\nServer:: CustomBinding/DefaultValues";
+
+        StringBuilder errorBuilder = new StringBuilder();
+
+        try
+        {
+            CustomBinding binding = new CustomBinding(new TextMessageEncodingBindingElement(), new HttpsTransportBindingElement());
+            ScenarioTestHelpers.RunBasicEchoTest(binding, Endpoints.HttpsSoap12_Address, variationDetails, errorBuilder);
+        }
+        catch (Exception ex)
+        {
+            errorBuilder.AppendLine(String.Format("Unexpected exception was caught: {0}", ex.ToString()));
+        }
+
+        Assert.True(errorBuilder.Length == 0, String.Format("Test Case: {0} FAILED with the following errors: {1}", testCaseName, errorBuilder));
+    }
+}
