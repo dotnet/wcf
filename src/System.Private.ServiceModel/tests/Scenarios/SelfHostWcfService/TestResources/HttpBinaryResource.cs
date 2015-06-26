26679d4ffe6dd427e658475f16d09f7ccdb1d89f
Dustin Metzgar <dmetzgar@microsoft.com>
2015-06-26 18:15:36 +0000

    Added encoders tests, which includes a tcp endpoint.

---
+++ src/System.Private.ServiceModel/tests/Scenarios/SelfHostWcfService/TestResources/HttpBinaryResource.cs
@@ -0,0 +1,19 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Channels;
+
+namespace WcfService.TestResources
+{
+    internal class HttpBinaryResource : ResourceController<WcfService, IWcfService>
+    {
+        protected override string Protocol { get { return "http"; } }
+
+        protected override string Address { get { return "http-binary"; } }
+
+        protected override string Port { get { return "8081"; } }
+
+        protected override Binding GetBinding()
+        {
+            return new CustomBinding(new BinaryMessageEncodingBindingElement(), new HttpTransportBindingElement());
+        }
+    }
+}
---
+++ src/System.Private.ServiceModel/tests/Scenarios/SelfHostWcfService/TestResources/TcpNoSecurityTextResource.cs
@@ -0,0 +1,19 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Channels;
+
+namespace WcfService.TestResources
+{
+    internal class TcpNoSecurityTextResource : ResourceController<WcfService, IWcfService>
+    {
+        protected override string Protocol { get { return "net.tcp"; } }
+
+        protected override string Address { get { return "tcp-custombinding-nosecurity-text"; } }
+
+        protected override string Port { get { return "809"; } }
+
+        protected override Binding GetBinding()
+        {
+            return new CustomBinding(new TextMessageEncodingBindingElement(), new TcpTransportBindingElement() { PortSharingEnabled = false });
+        }
+    }
+}
