1e74de81f8925b403c588e6ad926c5764f7d3722
Dustin Metzgar <dmetzgar@microsoft.com>
2015-06-26 01:26:25 +0000

    Didn't see that these files were not being tracked with the last commit.

---
+++ src/System.Private.ServiceModel/tests/Scenarios/SelfHostWcfService/TestResources/HttpSoap11Resource.cs
@@ -0,0 +1,23 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Text;
+
+namespace WcfService.TestResources
+{
+    internal class HttpSoap11Resource : ResourceController<WcfService, IWcfService>
+    {
+        protected override string Protocol { get { return "http"; } }
+
+        protected override string Address { get { return "http-soap11"; } }
+
+        protected override string Port { get { return "8081"; } }
+
+        protected override Binding GetBinding()
+        {
+            var binding = new CustomBinding();
+            binding.Elements.Add(new TextMessageEncodingBindingElement(MessageVersion.Soap11, Encoding.UTF8));
+            binding.Elements.Add(new HttpTransportBindingElement());
+            return binding;
+        }
+    }
+}
---
+++ src/System.Private.ServiceModel/tests/Scenarios/SelfHostWcfService/TestResources/HttpsSoap11Resource.cs
@@ -0,0 +1,23 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Text;
+
+namespace WcfService.TestResources
+{
+    internal class HttpsSoap11Resource : ResourceController<WcfService, IWcfService>
+    {
+        protected override string Protocol { get { return "https"; } }
+
+        protected override string Address { get { return "https-soap11"; } }
+
+        protected override string Port { get { return "44285"; } }
+
+        protected override Binding GetBinding()
+        {
+            var binding = new CustomBinding();
+            binding.Elements.Add(new TextMessageEncodingBindingElement(MessageVersion.Soap11, Encoding.UTF8));
+            binding.Elements.Add(new HttpsTransportBindingElement());
+            return binding;
+        }
+    }
+}
