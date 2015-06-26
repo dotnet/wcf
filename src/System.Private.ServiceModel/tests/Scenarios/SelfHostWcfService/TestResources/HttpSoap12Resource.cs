21ac3da912330a12bece62a1d979358bffa95c27
Dustin Metzgar <dmetzgar@microsoft.com>
2015-06-26 01:26:25 +0000

    Didn't see that these files were not being tracked with the last commit.

---
+++ src/System.Private.ServiceModel/tests/Scenarios/SelfHostWcfService/TestResources/HttpSoap12Resource.cs
@@ -0,0 +1,23 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Text;
+
+namespace WcfService.TestResources
+{
+    internal class HttpSoap12Resource : ResourceController<WcfService, IWcfService>
+    {
+        protected override string Protocol { get { return "http"; } }
+
+        protected override string Address { get { return "http-soap12"; } }
+
+        protected override string Port { get { return "8081"; } }
+
+        protected override Binding GetBinding()
+        {
+            var binding = new CustomBinding();
+            binding.Elements.Add(new TextMessageEncodingBindingElement(MessageVersion.Soap12WSAddressing10, Encoding.UTF8));
+            binding.Elements.Add(new HttpTransportBindingElement());
+            return binding;
+        }
+    }
+}
