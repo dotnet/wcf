170350e9ed608a71566dd98f6655e8fccf08137c
Ron Cain <roncain@microsoft.com>
2015-08-06 14:16:34 -0700

    Allow Bridge URL to be other than localHost
    Allows the Bridge to be started eith remote access enabled
    or disabled.
    
    Also updates the launch scripts to allow port, host, and allowRemote
    options to be passed through to Bridge.exe
    
    Fixes #248

--- src/System.Private.ServiceModel/tools/setupfiles/SetupWCFTestService.cmd
+++ src/System.Private.ServiceModel/tools/setupfiles/SetupWCFTestService.cmd
@@ -1,10 +1,12 @@
 echo off
 setlocal
+
 REM elevated window does not set current directory correctly. 
-REM Workaround it by passing the current directory around
+REM Workaround it by passing the current directory as 1st parameter
 pushd %1
+
 certutil -addstore -f root RootCATest.cer
 certutil -f -p test -importpfx "WcfTestServer.pfx"
 netsh http add sslcert ipport=0.0.0.0:44285 certhash=1d85a3f6cd2c022c5ca54e5cb200a47f89ba0d3d appid={00000000-0000-0000-0000-000000000000}
-start powershell -ExecutionPolicy Bypass -File ..\test\Bridge\bin\ensureBridge.ps1
-exit
+start powershell -ExecutionPolicy Bypass -File ..\test\Bridge\bin\ensureBridge.ps1 %2 %3 %4 %5 %6 %7
+exit /b
--- src/System.Private.ServiceModel/tools/test/Bridge/Bridge/Program.cs
+++ src/System.Private.ServiceModel/tools/test/Bridge/Bridge/Program.cs
@@ -13,29 +13,37 @@ namespace Bridge
 
         private static void Main(string[] args)
         {
-            int portNumber = DefaultPortNumber;
+            CommandLineArguments commandLineArgs = new CommandLineArguments(args);
 
-            if (args.Length > 0)
+            string hostFormatString = "http://{0}:{1}";
+            string owinAddress = String.Format(hostFormatString, commandLineArgs.AllowRemote ? "+" : "localhost", commandLineArgs.Port);
+            string visibleHost = (commandLineArgs.AllowRemote) ? Environment.MachineName : "localhost";
+            string visibleAddress = String.Format(hostFormatString, visibleHost, commandLineArgs.Port);
+
+            OwinSelfhostStartup.Startup(owinAddress);
+
+            Console.WriteLine("The Bridge is listening at {0} with remote access {1}", visibleAddress, commandLineArgs.AllowRemote ? "enabled" : "disabled");
+
+            Test(visibleHost, commandLineArgs.Port);
+
+            while (true)
             {
-                if (!int.TryParse(args[0], out portNumber))
+                Console.WriteLine("Type \"exit\" to stop the Bridge.");
+                string answer = Console.ReadLine();
+                if (String.Equals(answer, "exit", StringComparison.OrdinalIgnoreCase))
                 {
-                    portNumber = DefaultPortNumber;
+                    break;
                 }
             }
-
-            string baseAddress = "http://localhost:" + portNumber;
-            OwinSelfhostStartup.Startup(baseAddress);
-            Console.WriteLine("Sample Usage:");
-            //curl --request PUT 'http://<baseAddress>/resource' -H "Content-Type:application/json" -H "Accept: application/json" --data "{name:'Bridge.Commands.Hostname'}"
-            Console.WriteLine("curl --request PUT '{0}/resource' -H \"Content-Type:application/json\" -H \"Accept: application/json\" --data \"{{name:'Bridge.Commands.Hostname'}}\"", baseAddress);
-            Test(portNumber);
-            Console.ReadLine();
         }
 
         [Conditional("DEBUG")]
-        private static void Test(int portNumber)
+        private static void Test(string hostName, int portNumber)
         {
-            ProcessStartInfo procStartInfo = new ProcessStartInfo("powershell.exe", "-ExecutionPolicy Bypass -File " + Path.GetFullPath("ensureBridge.ps1") + " " + portNumber);
+            Console.WriteLine("Self-testing the Bridge...");
+            string commandLine = String.Format("-ExecutionPolicy Bypass -File {0} -portNumber {1} -hostName {2}", 
+                                            Path.GetFullPath("ensureBridge.ps1"), portNumber, hostName);
+            ProcessStartInfo procStartInfo = new ProcessStartInfo("powershell.exe", commandLine);
             procStartInfo.RedirectStandardOutput = true;
             procStartInfo.UseShellExecute = false;
             procStartInfo.CreateNoWindow = true;
@@ -45,6 +53,74 @@ namespace Bridge
             proc.WaitForExit();
             string result = proc.StandardOutput.ReadToEnd();
             Console.WriteLine("Result from Test: " + result);
+        }
+
+        class CommandLineArguments
+        {
+            private const int DefaultPortNumber = 44283;
+            private const bool DefaultAllowRemote = false;
+
+            public CommandLineArguments(string[] args)
+            {
+                Port = DefaultPortNumber;
+                AllowRemote = DefaultAllowRemote;
+                bool success = Parse(args);
+                if (!success)
+                {
+                    ShowUsage();
+                    Environment.Exit(-1);
+                }
+            }
+
+            public int Port { get; private set; }
+            public bool AllowRemote { get; private set; }
+
+            private bool Parse(string[] args)
+            {
+                foreach (string arg in args)
+                {
+                    if (!arg.StartsWith("/") && !arg.StartsWith("-"))
+                    {
+                        return false;
+                    }
+                    string[] argAndValue = arg.Substring(1).Split(':');
+                    if (argAndValue.Length == 0)
+                    {
+                        return false;
+                    }
+                    if (String.Equals(argAndValue[0], "port", StringComparison.OrdinalIgnoreCase))
+                    {
+                        if (argAndValue.Length < 2)
+                        {
+                            return false;
+                        }
+                        int port = 0;
+                        if (!int.TryParse(argAndValue[1], out port))
+                        {
+                            return false;
+                        }
+                        Port = port;
+                    }
+                    else if (String.Equals(argAndValue[0], "allowRemote", StringComparison.OrdinalIgnoreCase))
+                    {
+                        AllowRemote = true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
+            private void ShowUsage()
+            {
+                Console.WriteLine("Starts a new instance of the Bridge.  Usage is:");
+                Console.WriteLine("Bridge.exe [/port:nnn] [/allowRemote]");
+                Console.WriteLine("   /port:nnn     Listening port for the bridge (default is {0}", DefaultPortNumber);
+                Console.WriteLine("   /allowRemote  If specified, allows access from other than localHost (default is localhost only)");
+            }
         }
     }
 }
