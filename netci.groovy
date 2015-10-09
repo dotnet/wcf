bdb87bd20050fe3e73a5045ac6da3049493cba08
Matt Mitchell <mmitche@microsoft.com>
2015-10-09 09:21:27 -0700

    Fix Linux PR test jobs so that they launch the right builds

--- netci.groovy
+++ netci.groovy
@@ -184,12 +184,15 @@ def project = 'dotnet/wcf'
         def jobName = "linux_${configurationJobName}"
         
         def linuxFlowJob = buildFlowJob(Utilities.getFullJobName(project, jobName, isPR)) {
+            def buildJobName = Utilities.getFolderName(project) + '/' + Utilities.getFullJobName(project, jobName + '_bld', isPR)
+            def testJobName = Utilities.getFolderName(project) + '/' + Utilities.getFullJobName(project, jobName + '_tst', isPR)
+            
             buildFlow('''
 // Build the Linux _bld job
-def linuxBuildJob = build(\"dotnet_wcf/''' + jobName + '''_bld\")
+def linuxBuildJob = build(\"''' + buildJobName + '''\")
 // Pass this to the test job.  Include the parameters
 build(params + [WCF_LINUX_BUILD_NUMBER: linuxBuildJob.build.number], 
-    \"dotnet_wcf/linux_''' + configurationJobName + '''_tst\")
+    \"''' + testJobName + '''\")
             ''')
 
             // Needs a workspace
