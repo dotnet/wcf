cf9ef23030fa7d5f5afdd34af11e5aa1ca2d0f80
Stephen Bonikowsky <stebon@microsoft.com>
2016-07-11 11:49:54 -0700

    Latest CoreFx changes to various shared scripts.
    * Covers numerous small changes.

--- run-test.cmd
+++ run-test.cmd
@@ -2,14 +2,14 @@
 
 :: To run tests outside of MSBuild.exe
 :: %1 is the path to the tests\<OSConfig> folder
+:: %2 is the path to the packages folder
 
 pushd %1
 
 FOR /D %%F IN (*.Tests) DO (
 	IF EXIST %%F\netcoreapp1.0 (
 		pushd %%F\netcoreapp1.0
-		@echo "corerun.exe xunit.console.netcore.exe %%F.dll -xml testResults.xml -notrait category=outerloop -notrait category=failing -notrait category=nonwindowstests -notrait Benchmark=true -notrait category=IgnoreForCI"
-		corerun.exe xunit.console.netcore.exe %%F.dll -xml testResults.xml -notrait category=outerloop -notrait category=failing -notrait category=nonwindowstests -notrait Benchmark=true -notrait category=IgnoreForCI
+		CALL RunTests.cmd %2
 		popd
 	)
 )
