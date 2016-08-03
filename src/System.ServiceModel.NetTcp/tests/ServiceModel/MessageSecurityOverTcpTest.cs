433697a4862c818ad538c48628920da45bc4d1c5
Ron Cain <roncain@microsoft.com>
2016-08-03 09:07:49 -0700

    Change some NetTcp unit tests to check PlatformNotSupportedException
    The test that formerly was marked with an IssueAttribute because
    it is not supported has been converted to a test that verifies
    we throw PlatformNotSupportedException.  And the IssueAttribute
    was removed.
    
    2 additional tests were added to assert we get the expected behavior
    from the default ctor and when setting MessageCredentialType to None.

--- src/System.ServiceModel.NetTcp/tests/ServiceModel/MessageSecurityOverTcpTest.cs
+++ src/System.ServiceModel.NetTcp/tests/ServiceModel/MessageSecurityOverTcpTest.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using System.ServiceModel;
 using Xunit;
 using Infrastructure.Common;
@@ -19,6 +20,19 @@ public static class MessageSecurityOverTcpTest
     }
 
 #if FULLXUNIT_NOTSUPPORTED
+    [Fact]
+#endif
+    [WcfFact]
+    public static void Ctor_Default_Properties_Not_Supported()
+    {
+        MessageSecurityOverTcp msot = new MessageSecurityOverTcp();
+        Assert.Throws <PlatformNotSupportedException>(() =>
+        {
+            MessageCredentialType unused = msot.ClientCredentialType;
+        });
+    }
+
+#if FULLXUNIT_NOTSUPPORTED
     [Theory]
 #endif
     [WcfTheory]
@@ -26,13 +40,26 @@ public static class MessageSecurityOverTcpTest
     [InlineData(MessageCredentialType.IssuedToken)]
     [InlineData(MessageCredentialType.UserName)]
     [InlineData(MessageCredentialType.Windows)]
-    [Issue(1434, Framework = FrameworkID.NetCore | FrameworkID.NetNative)]
-    public static void ClientCredentialType_Property(MessageCredentialType credentialType)
+    public static void ClientCredentialType_Property_Values_Not_Supported(MessageCredentialType credentialType)
+    {
+        MessageSecurityOverTcp msot = new MessageSecurityOverTcp();
+        Assert.Throws<PlatformNotSupportedException>(() =>
+        {
+            msot.ClientCredentialType = credentialType;
+        });
+    }
+
+#if FULLXUNIT_NOTSUPPORTED
+    [Theory]
+#endif
+    [WcfTheory]
+    [InlineData(MessageCredentialType.None)]
+    public static void ClientCredentialType_Property_Values_Supported(MessageCredentialType credentialType)
     {
         MessageSecurityOverTcp msot = new MessageSecurityOverTcp();
         msot.ClientCredentialType = credentialType;
         MessageCredentialType actual = msot.ClientCredentialType;
         Assert.True(actual == credentialType,
-                    string.Format("ClientCredentialType returned {0} but expected {1}", credentialType, actual));
+                    string.Format("ClientCredentialType returned '{0}' but expected '{1}'", credentialType, actual));
     }
 }
