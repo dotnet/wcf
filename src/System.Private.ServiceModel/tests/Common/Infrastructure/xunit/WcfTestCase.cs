7821e054103e4bd4a74e5656f3f5fff690ec3185
hongdai <hongdai@microsoft.com>
2016-08-05 15:16:07 -0700

    Emit WCF ETW events whenever a test fail
    * This is to help analyze test failures, especially on non window
    platforms

--- src/System.Private.ServiceModel/tests/Common/Infrastructure/xunit/WcfTestCase.cs
+++ src/System.Private.ServiceModel/tests/Common/Infrastructure/xunit/WcfTestCase.cs
@@ -2,7 +2,11 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Xunit.Abstractions;
@@ -17,6 +21,8 @@ namespace Infrastructure.Common
         private readonly string _skippedReason;
         private readonly bool _isTheory;
         private readonly IMessageSink _diagnosticMessageSink;
+
+        static TestEventListener s_testListener = new TestEventListener(new List<string>() { "Microsoft-Windows-Application Server-Applications" }, EventLevel.Verbose);
 
         internal WcfTestCase(IXunitTestCase testCase, string skippedReason = null, bool isTheory = false, IMessageSink diagnosticMessageSink = null)
         {
@@ -44,13 +50,51 @@ namespace Infrastructure.Common
 
         public void Deserialize(IXunitSerializationInfo info) { _testCase.Deserialize(info); }
 
-        public Task<RunSummary> RunAsync(
+        public async Task<RunSummary> RunAsync(
             IMessageSink diagnosticMessageSink, IMessageBus messageBus, object[] constructorArguments,
             ExceptionAggregator aggregator, CancellationTokenSource cancellationTokenSource)
         {
-            return _isTheory
-                ? new XunitTheoryTestCaseRunner(this, DisplayName, _skippedReason, constructorArguments, _diagnosticMessageSink, messageBus, aggregator, cancellationTokenSource).RunAsync()
-                : new XunitTestCaseRunner(this, DisplayName, _skippedReason, constructorArguments, TestMethodArguments, messageBus, aggregator, cancellationTokenSource).RunAsync();
+            ConcurrentQueue<EventWrittenEventArgs> events = new ConcurrentQueue<EventWrittenEventArgs>();
+	        s_testListener.EventWritten = events.Enqueue;
+
+            RunSummary runsummary = await (_isTheory ? new XunitTheoryTestCaseRunner(this, DisplayName, _skippedReason, constructorArguments, _diagnosticMessageSink, messageBus, aggregator, cancellationTokenSource).RunAsync()
+                                                     : new XunitTestCaseRunner(this, DisplayName, _skippedReason, constructorArguments, TestMethodArguments, messageBus, aggregator, cancellationTokenSource).RunAsync());
+
+            s_testListener.EventWritten = null;
+            if (runsummary.Failed > 0)
+            {
+                StringBuilder etwOutput = new StringBuilder();
+                etwOutput.AppendLine(string.Format("---ETW Trace for Test {0} Begins---", DisplayName));
+                foreach (var item in events)
+                {
+                    try
+                    {
+                        etwOutput.AppendLine(string.Format(DisplayName + ":" + item.Message, item.Payload));
+                    }
+                    //The mumber of parameters in Payload does not match the number of arguments in the item.Message and thus cause a
+                    // FormatException occationally, In this case, we catch and output all items in the payload and the Message without formatting the message.
+                    // https://github.com/dotnet/wcf/issues/1440 is opened to investigate the root cause of the mismatch exception.
+                    catch (Exception e)
+                    {
+                        etwOutput.AppendLine(String.Format("ETW message encountered exception '{0}' using DisplayName '{1}', format '{2}' and {3} payload items",
+                                             e.Message, DisplayName, item.Message, item.Payload.Count));
+
+                        etwOutput.AppendLine(string.Format("ETW message: {0}, payload below was received", item.Message));
+                        foreach (object payloadPara in item.Payload)
+                        {
+                            if (payloadPara != null)
+                            {
+                                etwOutput.AppendLine(string.Format("{0}: {1}", DisplayName, payloadPara.ToString()));
+                            }
+                        }
+                    }
+                }
+                etwOutput.AppendLine(string.Format("---ETW Trace for Test {0} Ends---", DisplayName));
+
+                Console.WriteLine(etwOutput);
+            }
+
+            return runsummary;
         }
 
         public void Serialize(IXunitSerializationInfo info) { _testCase.Serialize(info); }
