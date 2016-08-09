7ba43bd1c95e5386ca7b88c5703a69b2f872f661
Ron Cain <roncain@microsoft.com>
2016-08-09 08:55:33 -0700

    Adds IssueAttribute for NET Native inadvertantly omitted
    When converting all ActiveIssueAttributes to IssueAttribute in
    preparation for UWP testing, one was accidentally omitted. This
    test was supposed to be skipped due to this known issue, not
    executed and counted as a failure.

--- src/System.Private.ServiceModel/tests/Scenarios/Security/TransportSecurity/Https/HttpsTests.cs
+++ src/System.Private.ServiceModel/tests/Scenarios/Security/TransportSecurity/Https/HttpsTests.cs
@@ -96,6 +96,7 @@ public partial class HttpsTests : ConditionalWcfTest
                nameof(Peer_Certificate_Installed),
                nameof(SSL_Available))]
     [Issue(1295, OS = OSID.AnyUnix)] // A libcurl built with OpenSSL is required.
+    [Issue(959, Framework = FrameworkID.NetNative)] // Server certificate validation not supported in NET Native
     [OuterLoop]
     // Asking for PeerTrust alone should throw SecurityNegotiationException
     // if the certificate is not in the TrustedPeople store.  For this test
