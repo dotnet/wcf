3a158b5450f931895eab29556c347f1f4fab91c6
Jason Pang <jasonpa@microsoft.com>
2016-09-30 15:49:38 -0700

    Work around X509Certificate2 copy constructor omission
    In .NET Core, the X509Certificate2 copy constructor was missing; where WCF code
    needed to clone a certificate, we replaced the references to the copy constructor
    with code that uses the certificate.Handle instead
    
    However, in Linux, OpenSSL does not provide the private key when using this handle,
    meaning the private key goes missing in all subsequent copies; as such, client cert
    authentication doesn't work.
    
    This is a stopgap solution only, as we await the inclusion of the X509Certificate2
    copy ctor in a future .NET Core version

---
+++ src/System.Private.ServiceModel/src/Extensions/X509Certificate2Extensions.cs
@@ -0,0 +1,40 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Security.Cryptography.X509Certificates;
+
+namespace System.ServiceModel
+{
+    internal static class X509Certificate2Extensions
+    {
+        // dotnet/wcf#1574 - This is a workaround for dotnet/corefx#12214
+        // The copy constructor was removed in .NET Core, this is the workaround to allow us to make copies of certificates
+
+        internal static X509Certificate2 CloneCertificateInternal(this X509Certificate2 certificateToClone)
+        {
+#if TARGETS_WINDOWS
+            return new X509Certificate2(certificateToClone.Handle);
+#else
+            X509Certificate2Collection collection = new X509Certificate2Collection(certificateToClone);
+            X509Certificate2Collection copyCollection = collection.Find(X509FindType.FindByThumbprint, certificateToClone.Thumbprint, false);
+            return copyCollection[0];
+#endif
+        }
+
+        // dotnet/wcf#1574 - This is a workaround for dotnet/corefx#12214
+        internal static X509Certificate2 CloneCertificateInternal(this X509Certificate certificateToClone)
+        {
+#if TARGETS_WINDOWS
+            return new X509Certificate2(certificateToClone.Handle);
+#else
+            byte[] certificateBytes = certificateToClone.Export(X509ContentType.Pfx, string.Empty);
+            X509Certificate2 certificateToClone2 = new X509Certificate2(certificateBytes, string.Empty);
+
+            X509Certificate2Collection collection = new X509Certificate2Collection(certificateToClone2);
+            X509Certificate2Collection copyCollection = collection.Find(X509FindType.FindByThumbprint, certificateToClone2.Thumbprint, false);
+            return copyCollection[0];
+#endif
+        }
+    }
+}
