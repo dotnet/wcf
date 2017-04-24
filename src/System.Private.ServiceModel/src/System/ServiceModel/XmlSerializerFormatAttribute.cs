4722e964a41c8af326f0451dcfc92a262ad3acfa
Shin Mao <shmao@microsoft.com>
2017-04-24 13:13:40 -0700

    Add OperationFormatUse in S.P.SM.

--- src/System.Private.ServiceModel/src/System/ServiceModel/XmlSerializerFormatAttribute.cs
+++ src/System.Private.ServiceModel/src/System/ServiceModel/XmlSerializerFormatAttribute.cs
@@ -12,6 +12,8 @@ namespace System.ServiceModel
     {
         private bool _supportFaults = false;
         private OperationFormatStyle _style;
+        private bool _isStyleSet;
+        private OperationFormatUse _use;
 
         public bool SupportFaults
         {
@@ -26,12 +28,41 @@ namespace System.ServiceModel
             {
                 ValidateOperationFormatStyle(value);
                 _style = value;
+                _isStyleSet = true;
             }
         }
+        
+        public OperationFormatUse Use
+        {
+            get { return _use; }
+            set
+            {
+                ValidateOperationFormatUse(value);
+                _use = value;
+                if (!_isStyleSet && IsEncoded)
+                {
+                    Style = OperationFormatStyle.Rpc;
+                }
+            }
+        }
+        
+        internal bool IsEncoded
+        {
+            get { return _use == OperationFormatUse.Encoded; }
+            set { _use = value ? OperationFormatUse.Encoded : OperationFormatUse.Literal; }
+        }
 
-        static internal void ValidateOperationFormatStyle(OperationFormatStyle value)
+        internal static void ValidateOperationFormatStyle(OperationFormatStyle value)
         {
             if (!OperationFormatStyleHelper.IsDefined(value))
+            {
+                throw DiagnosticUtility.ExceptionUtility.ThrowHelperError(new ArgumentOutOfRangeException("value"));
+            }
+        }
+        
+        internal static void ValidateOperationFormatUse(OperationFormatUse value)
+        {
+            if (!OperationFormatUseHelper.IsDefined(value))
             {
                 throw DiagnosticUtility.ExceptionUtility.ThrowHelperError(new ArgumentOutOfRangeException("value"));
             }
