3a1f31c0de10f74751dd60d12c9d05830f8f31f7
Shin Mao <shmao@microsoft.com>
2017-05-04 11:05:15 -0700

    Refactor the tests.

--- src/System.Private.ServiceModel/tests/Scenarios/Contract/XmlSerializer/XmlSerializerFormatSoapTest.cs
+++ src/System.Private.ServiceModel/tests/Scenarios/Contract/XmlSerializer/XmlSerializerFormatSoapTest.cs
@@ -7,24 +7,24 @@ using System.ServiceModel.Channels;
 using System.Threading.Tasks;
 using Infrastructure.Common;
 using Xunit;
+using System;
 
 public static partial class XmlSerializerFormatTests
 {
-    private static readonly string s_basicSoapEndpointAddress = Endpoints.HttpBaseAddress_Basic_Soap;
-
     [WcfFact]
     [OuterLoop]
     public static void CombineString_XmlSerializerFormat_Soap()
     {
-        var binding = new BasicHttpBinding();
-        var endpointAddress = new EndpointAddress(s_basicSoapEndpointAddress);
-        var factory = new ChannelFactory<IWcfSoapService>(binding, endpointAddress);
-        IWcfSoapService serviceProxy = factory.CreateChannel();
+        RunWcfSoapServiceTest((serviceProxy) =>
+        {
+            // *** EXECUTE *** \\
+            string message1 = "hello";
+            string message2 = "world";
+            var response = serviceProxy.CombineStringXmlSerializerFormatSoap(message1, message2);
 
-        string message1 = "hello";
-        string message2 = "world";
-        var response = serviceProxy.CombineStringXmlSerializerFormatSoap(message1, message2);
-        Assert.Equal(message1 + message2, response);
+            // *** VALIDATE *** \\
+            Assert.Equal(message1 + message2, response);
+        });
     }
 
     [WcfFact]
@@ -32,30 +32,58 @@ public static partial class XmlSerializerFormatTests
     [ActiveIssue(1884)]
     public static void EchoComositeType_XmlSerializerFormat_Soap()
     {
-        var binding = new BasicHttpBinding();
-        var endpointAddress = new EndpointAddress(s_basicSoapEndpointAddress);
-        var factory = new ChannelFactory<IWcfSoapService>(binding, endpointAddress);
-        IWcfSoapService serviceProxy = factory.CreateChannel();
+        RunWcfSoapServiceTest((serviceProxy) =>
+        {
+            // *** EXECUTE *** \\
+            var value = new SoapComplexType() { BoolValue = true, StringValue = "hello" };
+            SoapComplexType response = serviceProxy.EchoComositeTypeXmlSerializerFormatSoap(value);
 
-        var value = new SoapComplexType() { BoolValue = true, StringValue = "hello" };
-        SoapComplexType response = serviceProxy.EchoComositeTypeXmlSerializerFormatSoap(value);
-        Assert.NotNull(response);
-        Assert.Equal(value.BoolValue, response.BoolValue);
-        Assert.Equal(value.StringValue, response.StringValue);
+            // *** VALIDATE *** \\
+            Assert.NotNull(response);
+            Assert.Equal(value.BoolValue, response.BoolValue);
+            Assert.Equal(value.StringValue, response.StringValue);
+        });
     }
 
     [WcfFact]
     [OuterLoop]
     public static void ProcessCustomerData_XmlSerializerFormat_Soap()
     {
-        var binding = new BasicHttpBinding();
-        var endpointAddress = new EndpointAddress(s_basicSoapEndpointAddress);
-        var factory = new ChannelFactory<IWcfSoapService>(binding, endpointAddress);
-        IWcfSoapService serviceProxy = factory.CreateChannel();
+        RunWcfSoapServiceTest((serviceProxy) =>
+        {
+            // *** EXECUTE *** \\
+            CustomerObject value = new CustomerObject() { Name = "MyName", Data = new AdditionalData() { Field = "Foo" } };
+            string response = serviceProxy.ProcessCustomerData(value);
 
-        CustomerObject value = new CustomerObject() { Name = "MyName", Data = new AdditionalData() { Field = "Foo" } };
-        string response = serviceProxy.ProcessCustomerData(value);
+            // *** VALIDATE *** \\
+            Assert.Equal("MyNameFoo", response);
+        });
+    }
 
-        Assert.Equal("MyNameFoo", response);
+    private static void RunWcfSoapServiceTest(Action<IWcfSoapService> testMethod)
+    {
+        BasicHttpBinding binding;
+        EndpointAddress endpointAddress;
+        ChannelFactory<IWcfSoapService> factory;
+        IWcfSoapService serviceProxy = null;
+
+        try
+        {
+            // *** SETUP *** \\
+            binding = new BasicHttpBinding();
+            endpointAddress = new EndpointAddress(Endpoints.HttpBaseAddress_Basic_Soap);
+            factory = new ChannelFactory<IWcfSoapService>(binding, endpointAddress);
+            serviceProxy = factory.CreateChannel();
+            testMethod(serviceProxy);
+
+            // *** CLEANUP *** \\
+            factory.Close();
+            ((ICommunicationObject)serviceProxy).Close();
+        }
+        finally
+        {
+            // *** ENSURE CLEANUP *** \\
+            ScenarioTestHelpers.CloseCommunicationObjects((ICommunicationObject)serviceProxy);
+        }
     }
 }
