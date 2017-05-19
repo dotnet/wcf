de7d883eebf3ff09c9df1fccfdc9e075927b6dcc
Stephen Bonikowsky <stebon@microsoft.com>
2017-05-19 10:16:03 -0700

    Initial changes to try using buildtools v2 for WCF

---
+++ src/System.Private.ServiceModel/src/TestFile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SystemServiceModelTest
+{
+    class Program
+    {
+        static void SomeMethod()
+        {
+            String myString = "Hello";
+            if (string.Equals(myString, "Hello"))
+            {
+            }
+        }
+    }
+}
