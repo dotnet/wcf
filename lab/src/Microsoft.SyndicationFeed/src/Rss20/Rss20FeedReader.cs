8d9b8360b5fc4a4160670d076ab3bc8817e05a61
Drago Draganov <dragod@microsoft.com>
2017-07-18 14:26:57 -0700

    Add SyndicationFeedReaderBase.

--- lab/src/Microsoft.SyndicationFeed/src/Rss20/Rss20FeedReader.cs
+++ lab/src/Microsoft.SyndicationFeed/src/Rss20/Rss20FeedReader.cs
@@ -2,17 +2,15 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
 using System.Threading.Tasks;
 using System.Xml;
 
 namespace Microsoft.SyndicationFeed
 {
-    public sealed class Rss20FeedReader : ISyndicationFeedReader
+    public class Rss20FeedReader : SyndicationFeedReaderBase
     {
         private readonly XmlReader _reader;
         private bool _knownFeed;
-        private bool _currentSet;
 
         public Rss20FeedReader(XmlReader reader) 
             : this(reader, new Rss20FeedFormatter())
@@ -20,193 +18,25 @@ namespace Microsoft.SyndicationFeed
         }
 
         public Rss20FeedReader(XmlReader reader, ISyndicationFeedFormatter formatter)
+            : base(reader, formatter)
         {
-            _reader = reader ?? throw new ArgumentNullException(nameof(reader));
-            Formatter = formatter ?? throw new ArgumentNullException(nameof(formatter));
-
-            ElementType = SyndicationElementType.None;
+            _reader = reader;
         }
 
-        public ISyndicationFeedFormatter Formatter { get; private set; }
-
-        public SyndicationElementType ElementType { get; private set; }
-
-        public string ElementName { get; private set; }
-
-        public async Task<bool> Read()
+        public override async Task<bool> Read()
         {
-            await InitRead();
-
-            if (_currentSet) {
-                //
-                // The reader is already advanced, return status
-                _currentSet = false;
-                return !_reader.EOF;
-            }
-            else {
-                if (ElementType != SyndicationElementType.None)
-                {
-                    await Skip();
-                    return !_reader.EOF;
-                }
-                else
-                {
-                    //
-                    // Advance the reader
-                    return await MoveNext(false);
-                }
-            }
-        }
-
-        public async Task<ISyndicationCategory> ReadCategory()
-        {
-            await EnsureRead();
-
-            if (ElementType != SyndicationElementType.Category)
+            if (!_knownFeed)
             {
-                throw new XmlException("Unknown Category");
+                await InitRead();
+                _knownFeed = true;
             }
 
-            ISyndicationCategory category = Formatter.ParseCategory(await XmlUtils.ReadOuterXmlAsync(_reader));
-
-            await MoveNext();
-
-            return category;
+            return await base.Read();
         }
-
-        public async Task<ISyndicationContent> ReadContent()
+        
+        protected override SyndicationElementType MapElementType(string elementName)
         {
-            await EnsureRead();
-
-            //
-            // Any element can be read as ISyndicationContent
-            if (ElementType == SyndicationElementType.None)
-            {
-                throw new XmlException("Unknown Content");
-            }
-
-            ISyndicationContent content = new SyndicationContent(await XmlUtils.ReadOuterXmlAsync(_reader));
-
-            await MoveNext();
-
-            return content;
-        }
-
-        public async Task<ISyndicationItem> ReadItem()
-        {
-            await EnsureRead();
-
-            if (ElementType != SyndicationElementType.Item)
-            {
-                throw new XmlException("Unknown Item");
-                //throw new XmlException(SR.GetString(SR.UnknownItemXml, reader.LocalName, reader.NamespaceURI));
-            }
-
-            ISyndicationItem item = Formatter.ParseItem(await XmlUtils.ReadOuterXmlAsync(_reader));
-
-            await MoveNext();
-
-            return item;
-        }
-
-        public async Task<ISyndicationLink> ReadLink()
-        {
-            await EnsureRead();
-
-            if (ElementType != SyndicationElementType.Link)
-            {
-                throw new XmlException("Unknown Link");
-                //throw new XmlException(SR.GetString(SR.UnknownItemXml, reader.LocalName, reader.NamespaceURI));
-            }
-
-            ISyndicationLink link = Formatter.ParseLink(await XmlUtils.ReadOuterXmlAsync(_reader));
-
-            await MoveNext();
-
-            return link;
-        }
-
-        public async Task<ISyndicationPerson> ReadPerson()
-        {
-            await EnsureRead();
-
-            if (ElementType != SyndicationElementType.Person)
-            {
-                throw new XmlException("Unknown Person");
-                //throw new XmlException(SR.GetString(SR.UnknownItemXml, reader.LocalName, reader.NamespaceURI));
-            }
-
-            ISyndicationPerson person = Formatter.ParsePerson(await XmlUtils.ReadOuterXmlAsync(_reader));
-
-            await MoveNext();
-
-            return person;
-        }
-
-        public async Task<ISyndicationImage> ReadImage()
-        {
-            await EnsureRead();
-
-            if (ElementType != SyndicationElementType.Image)
-            {
-                throw new XmlException("Unknown Image");
-                //throw new XmlException(SR.GetString(SR.UnknownItemXml, reader.LocalName, reader.NamespaceURI));
-            }
-
-            ISyndicationImage image = Formatter.ParseImage(await XmlUtils.ReadOuterXmlAsync(_reader));
-
-            await MoveNext();
-
-            return image;
-        }
-
-
-        public async Task<T> ReadValue<T>()
-        {
-            ISyndicationContent content = await ReadContent();
-
-            if (!Formatter.TryParseValue(content.GetValue(), out T value))
-            {
-                throw new FormatException();
-            }
-
-            return value;
-        }
-
-        public async Task Skip()
-        {
-            await XmlUtils.SkipAsync(_reader);
-            await MoveNext(false);
-        }
-
-        private async Task<bool> MoveNext(bool setCurrent = true)
-        {
-            do
-            {
-                if (_reader.NodeType == XmlNodeType.Element)
-                {
-                    ElementType = MapElementType();
-                    ElementName = _reader.Name;
-
-                    _currentSet = setCurrent;
-
-                    return true;
-                }
-            }
-            while (await XmlUtils.ReadAsync(_reader));
-
-            //
-            // Reset
-            ElementType = SyndicationElementType.None;
-            ElementName = null;
-            _currentSet = false;
-
-            return false;
-        }
-
-        private SyndicationElementType MapElementType()
-        {
-            switch (_reader.Name)
+            switch (elementName)
             {
                 case Rss20Constants.ItemTag:
                     return SyndicationElementType.Item;
@@ -229,39 +59,28 @@ namespace Microsoft.SyndicationFeed
             }
         }
 
-        private async Task EnsureRead()
-        {
-            await InitRead();
-
-            if (ElementType == SyndicationElementType.None)
-            {
-                await Read();
-            }
-        }
 
         private async Task InitRead()
         {
-            if (!_knownFeed)
-            {
-                _knownFeed = _reader.IsStartElement(Rss20Constants.RssTag, Rss20Constants.Rss20Namespace)
-                                && _reader.GetAttribute(Rss20Constants.VersionTag).Equals(Rss20Constants.Version);
+            // Check <rss>
+            bool knownFeed = _reader.IsStartElement(Rss20Constants.RssTag, Rss20Constants.Rss20Namespace) &&
+                             _reader.GetAttribute(Rss20Constants.VersionTag).Equals(Rss20Constants.Version);
 
-                //
-                // Read <rss>
-                if (_knownFeed) {
-                    await XmlUtils.ReadAsync(_reader);
-                    _knownFeed = _reader.IsStartElement(Rss20Constants.ChannelTag, Rss20Constants.Rss20Namespace);
-                }
-
-                if (!_knownFeed)
-                {
-                    throw new XmlException("Unknown Feed");
-                    //throw new XmlException(SR.GetString(SR.UnknownFeedXml, reader.LocalName, reader.NamespaceURI));
-                }
-
-                // Read <channel>
+            if (knownFeed) {
+                // Read<rss>
                 await XmlUtils.ReadAsync(_reader);
+
+                // Check <channel>
+                knownFeed = _reader.IsStartElement(Rss20Constants.ChannelTag, Rss20Constants.Rss20Namespace);
             }
+
+            if (!knownFeed)
+            {
+                throw new XmlException("Unknown Rss Feed");
+            }
+
+            // Read <channel>
+            await XmlUtils.ReadAsync(_reader);
         }
     }
 }
