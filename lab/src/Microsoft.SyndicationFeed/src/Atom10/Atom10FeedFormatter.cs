59dc9160b5eca12cde224c87037cd5b78e4b2bd0
Gerardo Hurtado Lozano <kanegerardo_@hotmail.com>
2017-07-19 10:59:46 -0700

    Adding Atom10 Skeleton

---
+++ lab/src/Microsoft.SyndicationFeed/src/Atom10/Atom10FeedFormatter.cs
@@ -0,0 +1,41 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+
+namespace Microsoft.SyndicationFeed.Atom10
+{
+    public class Atom10FeedFormatter : ISyndicationFeedFormatter
+    {
+        public ISyndicationCategory ParseCategory(string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISyndicationImage ParseImage(string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISyndicationItem ParseItem(string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISyndicationLink ParseLink(string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISyndicationPerson ParsePerson(string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryParseValue<T>(string value, out T result)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ lab/src/Microsoft.SyndicationFeed/src/Atom10/Atom10FeedReader.cs
@@ -0,0 +1,22 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Xml;
+
+namespace Microsoft.SyndicationFeed.Atom10
+{
+    class Atom10FeedReader : SyndicationFeedReaderBase
+    {
+        public Atom10FeedReader(XmlReader reader, ISyndicationFeedFormatter formatter) 
+            : base(reader, formatter)
+        {
+        }
+
+        protected override SyndicationElementType MapElementType(string elementName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
