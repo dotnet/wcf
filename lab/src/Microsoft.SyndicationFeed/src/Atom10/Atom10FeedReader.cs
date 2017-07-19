ffe52c3a83886d7b9aced3e6acdb6c8e64782227
Gerardo Hurtado Lozano <kanegerardo_@hotmail.com>
2017-07-19 11:12:45 -0700

    Added Implementation of MapElementType in Atom10FeedReader

--- lab/src/Microsoft.SyndicationFeed/src/Atom10/Atom10FeedReader.cs
+++ lab/src/Microsoft.SyndicationFeed/src/Atom10/Atom10FeedReader.cs
@@ -9,14 +9,43 @@ namespace Microsoft.SyndicationFeed.Atom10
 {
     class Atom10FeedReader : SyndicationFeedReaderBase
     {
+        private readonly XmlReader _reader;
+        private bool _knownFeed;
+
+        public Atom10FeedReader(XmlReader reader)
+            : this(reader, new Atom10FeedFormatter())
+        {                
+        }
+
         public Atom10FeedReader(XmlReader reader, ISyndicationFeedFormatter formatter) 
             : base(reader, formatter)
         {
+            _reader = reader;
         }
 
         protected override SyndicationElementType MapElementType(string elementName)
         {
-            throw new NotImplementedException();
+            switch (elementName)
+            {
+                case Atom10Constants.EntryTag:
+                    return SyndicationElementType.Item;
+
+                case Atom10Constants.LinkTag:
+                    return SyndicationElementType.Link;
+
+                case Atom10Constants.CategoryTag:
+                    return SyndicationElementType.Category;
+
+                case Atom10Constants.LogoTag:
+                    return SyndicationElementType.Image;
+
+                case Atom10Constants.AuthorTag:
+                case Atom10Constants.ContributorTag:
+                    return SyndicationElementType.Person;
+
+                default:
+                    return SyndicationElementType.Content;
+            }
         }
     }
 }
