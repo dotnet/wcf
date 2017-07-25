019cb7d3f7681411c278a5e17123a6a009cb7d45
Gerardo Hurtado Lozano <kanegerardo_@hotmail.com>
2017-07-25 11:35:25 -0700

    Fixed bug in link parser. Failed to parse enclosure tags.

--- lab/src/Microsoft.SyndicationFeed/src/Rss20/Rss20FeedFormatter.cs
+++ lab/src/Microsoft.SyndicationFeed/src/Rss20/Rss20FeedFormatter.cs
@@ -202,14 +202,14 @@ namespace Microsoft.SyndicationFeed
             string type = string.Empty;
             TryParseValue(reader.GetAttribute("type"), out type);
             
-            reader.ReadStartElement();
+            //reader.ReadStartElement();
 
             //
             // Title
             string title = string.Empty;
             if (!reader.IsEmptyElement)
             {
-                title = reader.ReadContentAsString();
+                title = reader.ReadElementContentAsString();
 
                 // Url is the content, if not set as attribute
                 if (uri == null && !string.IsNullOrEmpty(title))
@@ -217,8 +217,10 @@ namespace Microsoft.SyndicationFeed
                     TryParseValue(title, out uri);
                 }
             }
-
-            reader.ReadEndElement();
+            else
+            {
+                reader.Skip();
+            }
 
             return new SyndicationLink(uri) {
                 Title = title,
