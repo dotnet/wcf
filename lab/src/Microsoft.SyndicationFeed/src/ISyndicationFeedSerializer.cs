3f41c0ae54a7e8a8236ca0fe0e2fb1a0fe9ed324
Gerardo Hurtado Lozano <kanegerardo_@hotmail.com>
2017-07-25 17:07:01 -0700

    Initial Interfaces for Writers

---
+++ lab/src/Microsoft.SyndicationFeed/src/ISyndicationFeedSerializer.cs
@@ -0,0 +1,22 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+
+namespace Microsoft.SyndicationFeed
+{
+    public interface ISyndicationFeedSerializer
+    {
+        string Serialize(ISyndicationContent content);
+
+        string Serialize(ISyndicationCategory category);
+
+        string Serialize(ISyndicationImage image);
+
+        string Serialize(ISyndicationItem item);
+
+        string Serialize(ISyndicationPerson person);
+
+        string Serialize(ISyndicationLink link);
+    }
+}
