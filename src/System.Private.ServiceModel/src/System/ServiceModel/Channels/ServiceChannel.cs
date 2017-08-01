f1ad0c362775d5bf0dbffb65c3b28b377fdda187
David Reher <david.reher@gmx.de>
2017-08-01 21:32:46 +0200

    Updated ServiceChannel to include WSAddressingAugust2004

--- src/System.Private.ServiceModel/src/System/ServiceModel/Channels/ServiceChannel.cs
+++ src/System.Private.ServiceModel/src/System/ServiceModel/Channels/ServiceChannel.cs
@@ -610,6 +610,18 @@ namespace System.ServiceModel.Channels
                         Exception error = new InvalidOperationException(text);
                         throw TraceUtility.ThrowHelperError(error, rpc.Request);
                     }
+ 
+                    if (this._messageVersion.Addressing == AddressingVersion.WSAddressingAugust2004)
+                    {
+                        EndpointAddress from = headers.From;
+                        if ((from != null) && !from.IsAnonymous && (localUri != from.Uri))
+                        {
+                            // FIXME: string text = SR.Format(SR.SFxRequestHasInvalidFromOnClient, from.Uri, localUri);
+                            string text = SR.Format("The request message has From='{0}' but IContextChannel.LocalAddress is '{1}'.  When ManualAddressing is false, these values must be the same, null, or EndpointAddress.AnonymousAddress.  Enable ManualAddressing or avoid setting From on the message.", faultTo.Uri, localUri);
+                            Exception error = new InvalidOperationException(text);
+                            throw TraceUtility.ThrowHelperError(error, rpc.Request);
+                        }
+                    }
                 }
             }
 
