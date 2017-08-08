59999a7b7aea8d9445ecc6057ba8f689142d36d4
Gerardo Hurtado Lozano <kanegerardo_@hotmail.com>
2017-08-08 13:07:32 -0700

    Changed AttomAttributes class to be private.

--- lab/src/Microsoft.SyndicationFeed/src/Atom/AtomAttributes.cs
+++ lab/src/Microsoft.SyndicationFeed/src/Atom/AtomAttributes.cs
@@ -4,7 +4,7 @@
 
 namespace Microsoft.SyndicationFeed.Atom
 {
-    public static class AtomAttributes
+    static class AtomAttributes
     {
         public const string Rel = "rel";
         public const string Href = "href";
