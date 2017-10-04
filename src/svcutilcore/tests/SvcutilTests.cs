02477c8cadf76efa3461ac3bb33730f083f0cf5a
Xing Mao <shmao@microsoft.com>
2017-10-04 10:38:00 -0700

    Added a test project for svcutil.

---
+++ src/svcutilcore/tests/SvcutilTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Microsoft.Tools.ServiceModel.SvcUtil.Tests
+{
+    public static class SvcutilTests
+    {
+        [Fact]
+        public static void SvcutilTest1()
+        {
+            Tool.Main(new string[] { "/t:xmlserializer", Assembly.GetExecutingAssembly().Location});
+        }
+    }
+}
