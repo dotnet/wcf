5023ae6921881a867a7bcd09b3b0d98196a30b01
Xing Mao <shmao@microsoft.com>
2017-10-05 15:12:57 -0700

    Make svcutil test project include xmlserializer tests.

--- src/svcutilcore/tests/SvcutilTests.cs
+++ src/svcutilcore/tests/SvcutilTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -13,7 +14,13 @@ namespace Microsoft.Tools.ServiceModel.SvcUtil.Tests
         [Fact]
         public static void SvcutilTest1()
         {
-            Tool.Main(new string[] { "/t:xmlserializer", Assembly.GetExecutingAssembly().Location});
+            string outputFile = Path.GetTempFileName() + ".cs";
+            Assert.False(File.Exists(outputFile));
+
+            Tool.Main(new string[] { "/t:xmlserializer", Assembly.GetExecutingAssembly().Location, $"/out:{outputFile}"});
+            Assert.True(File.Exists(outputFile));
+
+            File.Delete(outputFile);
         }
     }
 }
