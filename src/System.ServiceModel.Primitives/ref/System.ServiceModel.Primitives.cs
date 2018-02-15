0200d95c5b6c0f986478b8ecc5caefd8e54b4d23
Stephen Bonikowsky <stebon@microsoft.com>
2018-02-15 10:30:12 -0800

    Expose the Close method.
    * Fixes #2282
    * Including a simple unit test.

--- src/System.ServiceModel.Primitives/ref/System.ServiceModel.Primitives.cs
+++ src/System.ServiceModel.Primitives/ref/System.ServiceModel.Primitives.cs
@@ -133,6 +133,7 @@ namespace System.ServiceModel
         event System.EventHandler System.ServiceModel.ICommunicationObject.Opened { add { } remove { } }
         event System.EventHandler System.ServiceModel.ICommunicationObject.Opening { add { } remove { } }
         public void Abort() { }
+        public void Close() { }
         protected virtual TChannel CreateChannel() { return default(TChannel); }
         protected T GetDefaultValueForInitialization<T>() { return default(T); }
         protected void InvokeAsync(System.ServiceModel.ClientBase<TChannel>.BeginOperationDelegate beginOperationDelegate, object[] inValues, System.ServiceModel.ClientBase<TChannel>.EndOperationDelegate endOperationDelegate, System.Threading.SendOrPostCallback operationCompletedCallback, object userState) { }
