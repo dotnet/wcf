ee77e7eaf9c78a3df3cec01c69a293a157866400
Matt Connew <mconnew@microsoft.com>
2018-04-20 17:53:15 -0700

    Adding tests to verify Expect-100 continue is only sent when needed

--- src/System.Private.ServiceModel/tests/Scenarios/Security/TransportSecurity/Http/ClientCredentialTypeTests.4.1.0.cs
+++ src/System.Private.ServiceModel/tests/Scenarios/Security/TransportSecurity/Http/ClientCredentialTypeTests.4.1.0.cs
@@ -5,6 +5,7 @@
 // 
 
 using System;
+using System.Collections.Generic;
 using System.Net;
 using System.ServiceModel;
 using System.ServiceModel.Channels;
@@ -64,6 +65,69 @@ public static class Http_ClientCredentialTypeTests
 
             // *** VALIDATE *** \\
             Assert.True(result == testString, string.Format("Error: expected response from service: '{0}' Actual was: '{1}'", testString, result));
+
+            // *** CLEANUP *** \\
+            factory.Close();
+            ((ICommunicationObject)serviceProxy).Close();
+        }
+        finally
+        {
+            // *** ENSURE CLEANUP *** \\
+            ScenarioTestHelpers.CloseCommunicationObjects((ICommunicationObject)serviceProxy, factory);
+        }
+    }
+
+    [WcfFact]
+    [OuterLoop]
+    public static void HttpExpect100Continue_DigestAuthentication_True()
+    {
+        ChannelFactory<IWcfService> factory = null;
+        IWcfService serviceProxy = null;
+        BasicHttpBinding binding;
+
+        try
+        {
+            // *** SETUP *** \\
+            binding = new BasicHttpBinding(BasicHttpSecurityMode.TransportCredentialOnly);
+            binding.Security.Transport.ClientCredentialType = HttpClientCredentialType.Digest;
+            factory = new ChannelFactory<IWcfService>(binding, new EndpointAddress(Endpoints.Http_DigestAuth_NoDomain_Address));
+
+            string DigestUsernameHeaderName = "DigestUsername";
+            string DigestPasswordHeaderName = "DigestPassword";
+            string DigestRealmHeaderName = "DigestRealm";
+            string username = Guid.NewGuid().ToString("n").Substring(0, 8);
+            string password = Guid.NewGuid().ToString("n").Substring(0, 16);
+            string realm = Guid.NewGuid().ToString("n").Substring(0, 5);
+            factory.Credentials.HttpDigest.ClientCredential = new NetworkCredential(username, password, realm);
+
+            serviceProxy = factory.CreateChannel();
+
+            // *** EXECUTE *** \\
+            Dictionary<string, string> requestHeaders = null;
+            using (var scope = new OperationContextScope((IContextChannel)serviceProxy))
+            {
+                HttpRequestMessageProperty requestMessageProperty;
+                if (!OperationContext.Current.OutgoingMessageProperties.ContainsKey(HttpRequestMessageProperty.Name))
+                {
+                    requestMessageProperty = new HttpRequestMessageProperty();
+                    OperationContext.Current.OutgoingMessageProperties[HttpRequestMessageProperty.Name] = requestMessageProperty;
+                }
+                else
+                {
+                    requestMessageProperty = (HttpRequestMessageProperty)OperationContext.Current.OutgoingMessageProperties[HttpRequestMessageProperty.Name];
+                }
+
+                requestMessageProperty.Headers[DigestUsernameHeaderName] = username;
+                requestMessageProperty.Headers[DigestPasswordHeaderName] = password;
+                requestMessageProperty.Headers[DigestRealmHeaderName] = realm;
+
+                requestHeaders = serviceProxy.GetRequestHttpHeaders();
+            }
+
+            // *** VALIDATE *** \\
+            bool expectHeaderSent = requestHeaders.TryGetValue("Expect", out var expectHeader);
+            Assert.True(expectHeaderSent, "Expect header should have been sent but wasn't");
+            Assert.Equal("100-Continue", expectHeader, StringComparer.OrdinalIgnoreCase);
 
             // *** CLEANUP *** \\
             factory.Close();
