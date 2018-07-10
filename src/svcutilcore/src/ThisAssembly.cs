4283aed749ea96ff5756cad1c221eea3a94c4675
Cameron Taggart <cameron.taggart@gmail.com>
2018-07-10 17:11:30 -0500

    swtich to Project Sdk="Microsoft.NET.Sdk"

---
+++ src/svcutilcore/src/ThisAssembly.cs
@@ -0,0 +1,8 @@
+﻿namespace Microsoft.Tools.ServiceModel.SvcUtil.XmlSerializer
+{
+    class ThisAssembly
+    {
+        public const string Title = "";
+        public const string InformationalVersion = "";
+    }
+}
