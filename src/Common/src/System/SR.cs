a98b363259cb2677f2350a7f6c45c4c940a5a422
Stephen Bonikowsky <stebon@microsoft.com>
2019-04-25 15:08:39 -0700

    Missed a change in SR.cs
    * Another missed cahnge from the squashing.

--- src/Common/src/System/SR.cs
+++ src/Common/src/System/SR.cs
@@ -9,20 +9,6 @@ namespace System
 {
     internal partial class SR
     {
-        private static ResourceManager s_resourceManager;
-
-        private static ResourceManager ResourceManager
-        {
-            get
-            {
-                if (s_resourceManager == null)
-                {
-                    s_resourceManager = new ResourceManager(SR.ResourceType);
-                }
-                return s_resourceManager;
-            }
-        }
-
         // This method is used to decide if we need to append the exception message parameters to the message when calling SR.Format. 
         // by default it returns false.
         [MethodImpl(MethodImplOptions.NoInlining)]
