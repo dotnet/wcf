6717ff4df7814e17772b242ac7652b09943e517f
Carol Wang (Inspur Worldwide Services Ltd) <v-carwan@microsoft.com>
2019-06-13 15:35:37 +0800

    Issue1257 - test SecurityBindingElement.CreateUserNameOverTransportBindingElement()

---
+++ src/System.ServiceModel.Security/tests/ServiceModel/SecutityBindingElementTest.cs
@@ -0,0 +1,33 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+
+using System.ServiceModel.Channels;
+using System.ServiceModel.Security.Tokens;
+using Infrastructure.Common;
+using Xunit;
+
+public static class SecutityBindingElementTest
+{
+    [WcfFact]
+    public static void Method_CreateUserNameOverTransportBindingElement()
+    {
+        TransportSecurityBindingElement securityBindingElement = SecurityBindingElement.CreateUserNameOverTransportBindingElement();
+        Assert.True(securityBindingElement != null, "The created TransportSecurityBindingElement should not be null.");
+        Assert.True(securityBindingElement.IncludeTimestamp);
+        Assert.False(securityBindingElement.LocalClientSettings.DetectReplays);
+        Assert.True(securityBindingElement.EndpointSupportingTokenParameters.SignedEncrypted[0] is UserNameSecurityTokenParameters);
+
+        securityBindingElement.IncludeTimestamp = false;
+        securityBindingElement.SecurityHeaderLayout = SecurityHeaderLayout.Lax;
+        var binding = new CustomBinding();
+        binding.Elements.Add(securityBindingElement);
+        SecurityBindingElement element = binding.Elements.Find<SecurityBindingElement>();
+        Assert.True(element != null, "SecurityBindingElement added to binding elements should not be null.");
+        Assert.Equal(element, securityBindingElement);
+    }
+}
