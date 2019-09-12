61f53ca5f23292b6c569c9affc729e96e059f910
Sunny Chen (Inspur Worldwide Services Ltd) <v-fangfc@microsoft.com>
2019-09-11 19:30:03 -0700

    Fix format

--- src/System.ServiceModel.Security/tests/ServiceModel/SecureConversationSecurityTokenParametersTest.cs
+++ src/System.ServiceModel.Security/tests/ServiceModel/SecureConversationSecurityTokenParametersTest.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.IdentityModel.Tokens;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+
 using System.ServiceModel.Security.Tokens;
 using Infrastructure.Common;
 using Xunit;
-
 
 public static class SecureConversationSecurityTokenParametersTest
 {
