bd95e5bf4ef680cb0edf7f5d3a6bd1256cbdc2f7
Sunny Chen (Inspur Worldwide Services Ltd) <v-fangfc@microsoft.com>
2019-09-11 19:05:36 -0700

    Unit Test for SecureConversationSecurityTokenParameters class

---
+++ src/System.ServiceModel.Security/tests/ServiceModel/SecureConversationSecurityTokenParametersTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IdentityModel.Tokens;
+using System.ServiceModel.Security.Tokens;
+using Infrastructure.Common;
+using Xunit;
+
+
+public static class SecureConversationSecurityTokenParametersTest
+{
+    [WcfTheory]
+    [InlineData(false)]
+    [InlineData(true)]
+    public static void RequireCancellation_Property_Is_Settable(bool value)
+    {
+        SecureConversationSecurityTokenParameters scstr = new SecureConversationSecurityTokenParameters();
+        scstr.RequireCancellation = value;
+        Assert.Equal(value, scstr.RequireCancellation);
+    }
+}
