773e85b1d63ca4c58334d60cb7c199cedd8caa98
Sunny Chen (Inspur Worldwide Services Ltd) <v-fangfc@microsoft.com>
2019-09-23 00:13:28 -0700

    Add unit test for SecurityKeyEntropyMode.

---
+++ src/System.ServiceModel.Security/tests/ServiceModel/SecurityKeyEntropyModeTest.cs
@@ -0,0 +1,33 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+
+using System.ServiceModel.Security;
+using Infrastructure.Common;
+using Xunit;
+
+public static class SecurityKeyEntropyModeTest
+{
+    [WcfFact]
+    public static void Get_EnumMembers()
+    {
+        SecurityKeyEntropyMode clientEn = SecurityKeyEntropyMode.ClientEntropy;
+        Assert.Equal(SecurityKeyEntropyMode.ClientEntropy, clientEn);
+
+        SecurityKeyEntropyMode serverEn = SecurityKeyEntropyMode.ServerEntropy;
+        Assert.Equal(SecurityKeyEntropyMode.ServerEntropy, serverEn);
+
+        SecurityKeyEntropyMode combinedEn = SecurityKeyEntropyMode.CombinedEntropy;
+        Assert.Equal(SecurityKeyEntropyMode.CombinedEntropy, combinedEn);
+    }
+
+    [WcfTheory]
+    [InlineData(SecurityKeyEntropyMode.ClientEntropy, 0)]
+    [InlineData(SecurityKeyEntropyMode.ServerEntropy, 1)]
+    [InlineData(SecurityKeyEntropyMode.CombinedEntropy, 2)]
+    public static void TypeConvert_EnumToInt(SecurityKeyEntropyMode sem, int value)
+    {
+        Assert.Equal((int)sem, value);
+    }
+}
