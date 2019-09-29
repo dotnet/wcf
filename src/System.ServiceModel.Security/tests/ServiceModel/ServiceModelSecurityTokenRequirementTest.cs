151eaa93dc204bdae415b2cb6741a794a8bd3ae9
Sunny Chen (Inspur Worldwide Services Ltd) <v-fangfc@microsoft.com>
2019-09-29 00:05:32 -0700

    Unit Test for ChannelParametersCollectionProperty.

---
+++ src/System.ServiceModel.Security/tests/ServiceModel/ServiceModelSecurityTokenRequirementTest.cs
@@ -0,0 +1,22 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. 
+// The .NET Foundation licenses this file to you under the MIT license. 
+// See the LICENSE file in the project root for more information. 
+
+
+using System.ServiceModel.Security.Tokens;
+using Infrastructure.Common;
+using Xunit;
+
+public static class ServiceModelSecurityTokenRequirementTest
+{
+    [WcfFact]
+    public static void ChannelParametersCollectionProperty_DefaultValueIsSameAsNetFramework()
+    {
+        string cpcpValue = "http://schemas.microsoft.com/ws/2006/05/servicemodel/securitytokenrequirement/ChannelParametersCollection";
+
+        Assert.NotNull(ServiceModelSecurityTokenRequirement.ChannelParametersCollectionProperty);
+        Assert.Equal(cpcpValue, ServiceModelSecurityTokenRequirement.ChannelParametersCollectionProperty);
+    }
+}
+
+
