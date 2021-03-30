045ddb66d3f2a91c60cd3736e6f2756b5558bbdc
Matt Connew <matt.connew@outlook.com>
2021-03-29 23:15:32 -0700

    Remove unused CustomUserNameValidator (#4575)
    Fixed local test runs by adding 5.0 to global.json

--- src/System.Private.ServiceModel/tools/IISHostedWcfService/App_code/WcfUserNameService.cs
+++ src/System.Private.ServiceModel/tools/IISHostedWcfService/App_code/WcfUserNameService.cs
@@ -2,34 +2,11 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
-using System.IdentityModel.Selectors;
-using System.IdentityModel.Tokens;
-
 namespace WcfService
 {
-    public class CustomUserNameValidator : UserNamePasswordValidator
-    {
-        // This method validates users. It allows in two users, test1 and test2 
-        // with passwords 1tset and 2tset respectively.
-        public override void Validate(string userName, string password)
-        {
-            if (null == userName || null == password)
-            {
-                throw new ArgumentNullException();
-            }
-
-            // [SuppressMessage("Microsoft.Security", "CS002:SecretInNextLine", Justification="Net real secrets")]
-            if (!(userName == "test1" && password == "Mytestpwd1") && !(userName == "test2" && password == "test2pwd"))
-            {
-                throw new SecurityTokenException("Unknown Username or Incorrect Password");
-            }
-        }
-    }
-
     public class WcfUserNameService : IWcfCustomUserNameService
     {
-        public String Echo(String message)
+        public string Echo(string message)
         {
             return message;
         }
