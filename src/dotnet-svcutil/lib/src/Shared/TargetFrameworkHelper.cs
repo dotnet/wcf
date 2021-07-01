72790ad90f4feca901ca4667efaf7fdc36240f42
Carol Wang <v-carwan@microsoft.com>
2021-07-01 16:38:11 +0800

    Make case insensitive for targetframework comparison; make net6.0 use… (#4654)
    * Make case insensitive for targetframework comparison; make net6.0 use latest stable release of wcf pkg version.
    
    * Fix bug in GetLowestNetCoreVersion method.

--- src/dotnet-svcutil/lib/src/Shared/TargetFrameworkHelper.cs
+++ src/dotnet-svcutil/lib/src/Shared/TargetFrameworkHelper.cs
@@ -63,12 +63,6 @@ namespace Microsoft.Tools.ServiceModel.Svcutil
                 ProjectDependency.FromPackage("System.ServiceModel.Http", "4.8.*"    ),
                 ProjectDependency.FromPackage("System.ServiceModel.NetTcp", "4.8.*"  ),
                 ProjectDependency.FromPackage("System.ServiceModel.Security", "4.8.*"),
-            } },
-            {new Version("6.0"), new List<ProjectDependency> {
-                ProjectDependency.FromPackage("System.ServiceModel.Duplex", "4.9.*"  ),
-                ProjectDependency.FromPackage("System.ServiceModel.Http", "4.9.*"    ),
-                ProjectDependency.FromPackage("System.ServiceModel.NetTcp", "4.9.*"  ),
-                ProjectDependency.FromPackage("System.ServiceModel.Security", "4.9.*"),
             } }
         };
 
@@ -165,8 +159,16 @@ namespace Microsoft.Tools.ServiceModel.Svcutil
                     }
                     else
                     {
-                        // this is a target framework not yet known to the tool, use the latest known netcore version.
-                        netCoreVersion = TargetFrameworkHelper.NetStandardToNetCoreVersionMap.Values.LastOrDefault();
+                        // target framework is not the minumum standard supported netcore version but it is a known shipped netcore version.
+                        if (TargetFrameworkHelper.NetCoreVersionReferenceTable.ContainsKey(frameworkInfo.Version))
+                        {
+                            netCoreVersion = frameworkInfo.Version;
+                        }
+                        else
+                        {
+                            // target framework is not known to the tool, use the latest known netcore version.
+                            netCoreVersion = TargetFrameworkHelper.NetCoreVersionReferenceTable.Keys.LastOrDefault();
+                        }
                     }
 
                     if (targetVersion == null || targetVersion > netCoreVersion)
