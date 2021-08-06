d62d26cd819e6bde6dd8f3114291a4eae0356726
Zhaodong Tian <v-zhtian@microsoft.com>
2021-08-05 23:20:46 -0700

    Update the version of  packages

--- src/dotnet-svcutil/lib/src/DebugLogger.cs
+++ src/dotnet-svcutil/lib/src/DebugLogger.cs
@@ -35,6 +35,30 @@ namespace Microsoft.Tools.ServiceModel.Svcutil
             }
         }
 
+        private void LogMessage(string message, NuGet.Common.LogLevel level, bool logToUI = false)
+        {
+            if (this.EnableTracing || logToUI)
+            {
+                message = message?.Trim();
+
+                if (!string.IsNullOrEmpty(message))
+                {
+                    if (level == NuGet.Common.LogLevel.Error)
+                    {
+                        ToolConsole.WriteError(message, isTrace: !logToUI);
+                    }
+                    else if (level == NuGet.Common.LogLevel.Warning)
+                    {
+                        ToolConsole.WriteWarning(message, isTrace: !logToUI);
+                    }
+                    else
+                    {
+                        ToolConsole.WriteLine(message, logToUI ? LogTag.Information : LogTag.LogMessage);
+                    }
+                }
+            }
+        }
+
         #region ILogger
         public Task WriteMessageAsync(string message, bool logToUI)
         {
@@ -101,10 +125,31 @@ namespace Microsoft.Tools.ServiceModel.Svcutil
             LogMessage(data, LogTag.LogMessage);
         }
 
+        void NuGet.Common.ILogger.Log(NuGet.Common.ILogMessage message)
+        {
+            LogMessage(message.Message, message.Level);
+        }
+
+        Task NuGet.Common.ILogger.LogAsync(NuGet.Common.ILogMessage message)
+        {
+            return Task.Run(() => LogMessage(message.Message, message.Level));
+        }
+
+        Task NuGet.Common.ILogger.LogAsync(NuGet.Common.LogLevel level, string data)
+        {
+            return Task.Run(() => LogMessage(data, level));
+        }
+
+        void NuGet.Common.ILogger.Log(NuGet.Common.LogLevel level, string data)
+        {
+            LogMessage(data, level);
+        }
+
         public void LogErrorSummary(string data)
         {
             LogMessage(data, LogTag.Error);
         }
+
         #endregion
     }
 }
