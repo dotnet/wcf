6b0b042db6362f8cb0f92ad120c83b8feb2453cc
Brent Schmaltz <brentsch@ntdev.microsoft.com>
2021-10-16 15:44:34 -0700

    Add WSTrustChannelFactory and WSTrustChannel

---
+++ src/System.ServiceModel.Federation/tests/WSTrustChannelFactoryTest.cs
@@ -0,0 +1,17 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Infrastructure.Common;
+
+namespace System.ServiceModel.Federation.Tests
+{
+    public static class WSTrustChannelFactoryTest
+    {
+        [WcfFact]
+        public static void DefaultWSTrustChannelFactory()
+        {
+            WSTrustChannelFactory trustChannelFactory = new WSTrustChannelFactory(null, null);
+        }
+    }
+}
---
+++ src/System.ServiceModel.Federation/tests/WSTrustChannelTest.cs
@@ -0,0 +1,18 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Infrastructure.Common;
+using Microsoft.IdentityModel.Protocols.WsTrust;
+
+namespace System.ServiceModel.Federation.Tests
+{
+    public static class WSTrustChannelTest
+    {
+        [WcfFact]
+        public static void DefaultWSTrustChannel()
+        {
+            WSTrustChannel trustChannel = new WSTrustChannel(null);
+        }
+    }
+}
