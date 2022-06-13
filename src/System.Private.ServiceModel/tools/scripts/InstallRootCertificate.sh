3556ac81414e5dcab42c906ac0ab8f1d9f173764
Matt Connew <mconnew@users.noreply.github.com>
2022-06-13 16:48:05 -0700

    Adding UA to RootCert curl call to identify test variant in logs

--- src/System.Private.ServiceModel/tools/scripts/InstallRootCertificate.sh
+++ src/System.Private.ServiceModel/tools/scripts/InstallRootCertificate.sh
@@ -23,7 +23,7 @@ acquire_certificate()
     # Need to make a call as the original user as we need to write to the cert store for the current 
     # user, not as root
     echo "Making a call to '${__service_host}/TestHost.svc/RootCert' as user '$SUDO_USER'"
-    sudo -E -u $SUDO_USER $__curl_exe -o $__cafile "http://${__service_host}/TestHost.svc/RootCert?asPem=true" 
+    sudo -E -u $SUDO_USER $__curl_exe -o $__cafile -A "WcfCertDownloadScript/1.0 (${__os})" "http://${__service_host}/TestHost.svc/RootCert?asPem=true" 
     
     return $?
 }
