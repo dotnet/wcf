54f8eb17602ab9f3a5cd89131419a78bdd50bc5f
Matt Connew <mconnew@users.noreply.github.com>
2022-08-01 17:43:12 -0700

    Add capability to failfast when a test takes longer than expected

---
+++ src/System.Private.ServiceModel/tests/Common/Infrastructure/xunit/FailFastAfterAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace Infrastructure.Common
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class | AttributeTargets.Assembly, AllowMultiple = false)]
+    public class FailFastAfterAttribute : Attribute
+    {
+        public FailFastAfterAttribute(string durationString)
+        {
+            FailTime = TimeSpan.Parse(durationString);
+        }
+
+        public TimeSpan FailTime { get; }
+    }
+}
