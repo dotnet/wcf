e32c1286bfa51a4ebfe5243e4510e14c39e13fe4
Abdelwahab Afifi <v-aafifi@microsoft.com>
2022-08-06 17:49:06 +0200

    env script updates

--- src/System.Private.ServiceModel/tools/scripts/SetClientEnv-Linux.sh
+++ src/System.Private.ServiceModel/tools/scripts/SetClientEnv-Linux.sh
@@ -4,6 +4,9 @@ export Server_Domain_Joined=true
 export Ambient_Credentials_Available=true
 export Domain_Available=true
 export Explicit_Credentials_Available=true
+#set username and password
+#export ExplicitUserName= 
+#export ExplicitPassword=
 export SSL_Available=true
 export Root_Certificate_Installed=true
 export Client_Certificate_Installed=true
--- src/System.Private.ServiceModel/tools/scripts/SetClientEnv-Windows.cmd
+++ src/System.Private.ServiceModel/tools/scripts/SetClientEnv-Windows.cmd
@@ -4,6 +4,9 @@ setx Ambient_Credentials_Available true
 setx SPN_Available true
 setx NTLM_Available true
 setx Server_Domain_Joined true
+::set username and password
+::setx ExplicitUserName 
+::setx ExplicitPassword 
 setx Domain_Available true
 setx UPN_Available true
 setx Root_Certificate_Installed true
