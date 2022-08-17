8c06cb0b249576b69b7d738f298b3f0e4a7ef5f5
Zhaodong Tian <v-zhtian@microsoft.com>
2022-08-16 22:27:53 -0700

    Add perf testing and service develop with corewcf

---
+++ src/System.Private.ServiceModel/tests/Benchmarks/Perf/WCFCorePerfService/MyCustomValidator.cs
@@ -0,0 +1,18 @@
+﻿using CoreWCF.IdentityModel.Selectors;
+using System;
+using System.Threading.Tasks;
+
+namespace WCFCorePerfService
+{
+    public class MyCustomValidator : UserNamePasswordValidator
+    {
+        public override ValueTask ValidateAsync(string userName, string password)
+        {            
+            if (!string.IsNullOrEmpty(userName) && !string.IsNullOrEmpty(password))
+            {
+                return new ValueTask(Task.CompletedTask);
+            }
+            return new ValueTask(Task.FromException(new Exception("username and password cannot be empty")));
+        }
+    }
+}
---
+++ src/System.Private.ServiceModel/tests/Benchmarks/Perf/WCFCorePerfService/SayHello.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace WCFCorePerfService
+{
+    public class SayHello : ISayHello
+    {
+        public Task<string> HelloAsync(string name)
+        {
+            return Task.Factory.StartNew(() => { return name; });
+        }
+
+        public bool Cleanup(string name)
+        {
+            string command = $" advfirewall firewall delete rule name=\"{name}\"";
+            bool flag = false;
+            int code = ExecuteCommand(command, Environment.CurrentDirectory, TimeSpan.FromSeconds(20));
+            if (code == 0)
+                flag = true;
+
+            return flag;
+        }
+
+        private int ExecuteCommand(string command, string workingDirectory, TimeSpan timeout)
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = "netsh";
+            process.StartInfo.Arguments = command;
+            if (workingDirectory != null)
+            {
+                process.StartInfo.WorkingDirectory = workingDirectory;
+            }
+            process.StartInfo.UseShellExecute = false;
+            process.Start();
+            bool flag;
+            if (timeout.TotalMilliseconds >= Int32.MaxValue)
+            {
+                flag = process.WaitForExit(Int32.MaxValue);
+            }
+            else
+            {
+                flag = process.WaitForExit((int)timeout.TotalMilliseconds);
+            }
+            if (!flag)
+            {
+                process.Kill();
+            }
+
+            if (!flag)
+            {
+                throw new TimeoutException(string.Format("Command '{0}' was killed by timeout {1}.", new object[]
+                {
+                    command,
+                    timeout.ToString()
+                }));
+            }
+            return process.ExitCode;
+        }
+    }
+}
