32b23fc806e9c7d9f058d2a6879003eacb1b99fc
Abdelwahab Afifi <v-aafifi@microsoft.com>
2022-12-13 12:55:01 +0200

    Update script CertificatePrivateKey.ps1, add Cert Stores

--- src/System.Private.ServiceModel/tools/scripts/CertificatePrivateKeyPermissions.ps1
+++ src/System.Private.ServiceModel/tools/scripts/CertificatePrivateKeyPermissions.ps1
@@ -5,37 +5,33 @@ param (
 
 try
 {
-    #Get the list of WCF personal certificates having private key
-    $certs = Get-ChildItem Cert:\LocalMachine\My | where{$_.Issuer -like 'CN=DO_NOT_TRUST_WcfBridgeRootCA*' -and $_.HasPrivateKey -eq "True"}
+    $certStores = @('My','TrustedPeople')
 
-    #Open Certificate store and locate certificate based on provided thumbprint
-    $store = New-Object System.Security.Cryptography.X509Certificates.X509Store("My", "LocalMachine")
-    $store.Open("ReadWrite")
+    foreach ($certStore in $certStores)
+    {	
+        #Get the list of WCF personal certificates having private key
+        $certs = Get-ChildItem Cert:\LocalMachine\$certStore | where {$_.Issuer -like 'CN=DO_NOT_TRUST_WcfBridgeRootCA*' -and $_.HasPrivateKey -eq "True"}
 
-    foreach ($certificate in $certs)
-    {
-	    $thumbprint = $certificate.thumbprint
-	    $cert = $store.Certificates | where {$_.Thumbprint -eq $thumbprint}
+        #Locate certificate based on provided thumbprint
+        foreach ($cert in $certs)
+        {
+	        #Create new CSP object based on existing certificate provider and key name
+	        $csp = New-Object System.Security.Cryptography.CspParameters($cert.PrivateKey.CspKeyContainerInfo.ProviderType, $cert.PrivateKey.CspKeyContainerInfo.ProviderName, $cert.PrivateKey.CspKeyContainerInfo.KeyContainerName)
 
-	    #Create new CSP object based on existing certificate provider and key name
-	    $csp = New-Object System.Security.Cryptography.CspParameters($cert.PrivateKey.CspKeyContainerInfo.ProviderType, $cert.PrivateKey.CspKeyContainerInfo.ProviderName, $cert.PrivateKey.CspKeyContainerInfo.KeyContainerName)
+	        # Set flags and key security based on existing cert
+	        $csp.Flags = "UseExistingKey","UseMachineKeyStore"
+	        $csp.CryptoKeySecurity = $cert.PrivateKey.CspKeyContainerInfo.CryptoKeySecurity
+	        $csp.KeyNumber = $cert.PrivateKey.CspKeyContainerInfo.KeyNumber
 
-	    # Set flags and key security based on existing cert
-	    $csp.Flags = "UseExistingKey","UseMachineKeyStore"
-	    $csp.CryptoKeySecurity = $cert.PrivateKey.CspKeyContainerInfo.CryptoKeySecurity
-	    $csp.KeyNumber = $cert.PrivateKey.CspKeyContainerInfo.KeyNumber
+	        # Create new access rule for GenericRead
+	        $access = New-Object System.Security.AccessControl.CryptoKeyAccessRule($WcfServiceAccount, "GenericRead", "Allow")
+	        # Add access rule to CSP object
+	        $csp.CryptoKeySecurity.AddAccessRule($access)
 
-	    # Create new access rule for GenericRead
-	    $access = New-Object System.Security.AccessControl.CryptoKeyAccessRule($WcfServiceAccount, "GenericRead","Allow")
-	    # Add access rule to CSP object
-	    $csp.CryptoKeySecurity.AddAccessRule($access)
-
-	    #Create new CryptoServiceProvider object which updates Key with CSP information created/modified above
-	    $rsa2 = New-Object System.Security.Cryptography.RSACryptoServiceProvider($csp)
+	        #Create new CryptoServiceProvider object which updates Key with CSP information created/modified above
+	        $rsa2 = New-Object System.Security.Cryptography.RSACryptoServiceProvider($csp)
+        }
     }
-
-    #Close certificate store
-    $store.Close()	
     exit 0;
 }
 catch
