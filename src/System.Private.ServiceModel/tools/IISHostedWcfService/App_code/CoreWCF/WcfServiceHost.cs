1c14429aebd5a858cdc982d297fc8c28d09b0802
Abdelwahab Afifi <v-aafifi@microsoft.com>
2022-12-13 05:12:09 +0200

    Add CoreWCF project

---
+++ src/System.Private.ServiceModel/tools/IISHostedWcfService/App_code/CoreWCF/WcfServiceHost.cs
@@ -0,0 +1,49 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+#if NET
+
+using CoreWCF;
+using CoreWCF.Channels;
+
+namespace WcfService
+{
+    public class ServiceHost
+    {
+        private readonly Type _serviceType;
+        private readonly List<Endpoint> _endpoints = new List<Endpoint>();
+
+        public ServiceHost(Type serviceType, params Uri[] baseAddresses)
+        {
+            _serviceType = serviceType ?? throw new ArgumentNullException(nameof(serviceType));
+        }
+
+        public void AddServiceEndpoint(Type implementedContract, Binding binding, string address)
+        {
+            _endpoints.Add(new Endpoint
+            {
+                ContractType = implementedContract,
+                Binding = binding,
+                Address = address
+            });
+        }
+
+        public virtual void ApplyConfiguration(ServiceHostBase host)
+        {
+
+        }
+
+        public List<Endpoint> Endpoints => _endpoints;
+
+        public Type ServiceType => _serviceType;
+
+        public class Endpoint
+        {
+            public Type ContractType { get; set; }
+            public Binding Binding { get; set; }
+            public string Address { get; set; }
+        }
+    }
+}
+#endif
