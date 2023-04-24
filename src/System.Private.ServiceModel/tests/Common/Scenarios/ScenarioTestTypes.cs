ebd86b05dff17810f5bccf3bb0d79bfc972cdc36
Afifi <89879907+afifi-ins@users.noreply.github.com>
2023-04-24 08:00:58 +0200

    Add Mtom Upload Stream test code (#5034)

--- src/System.Private.ServiceModel/tests/Common/Scenarios/ScenarioTestTypes.cs
+++ src/System.Private.ServiceModel/tests/Common/Scenarios/ScenarioTestTypes.cs
@@ -947,6 +947,64 @@ public class WcfDuplexServiceCallback : IWcfDuplexServiceCallback, IWcfDuplexSer
     }
 }
 
+public class BloatedStream : Stream
+{
+    private readonly long _length;
+    private long _position;
+
+    public BloatedStream(long length)
+    {
+        _length = length;
+        _position = 0;
+    }
+
+    public override bool CanRead => true;
+
+    public override bool CanSeek => false;
+
+    public override bool CanWrite => false;
+
+    public override long Length => throw new NotSupportedException();
+
+    public override long Position
+    {
+        get => throw new NotSupportedException();
+        set => throw new NotSupportedException();
+    }
+
+    public override void Flush()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override int Read(byte[] buffer, int offset, int count)
+    {
+        if (_position < _length)
+        {
+            var numberOfBytesInThisRead = (int)Math.Min(count, _length - _position);
+            _position += numberOfBytesInThisRead;
+            return numberOfBytesInThisRead;
+        }
+
+        return 0;
+    }
+
+    public override long Seek(long offset, SeekOrigin origin)
+    {
+        throw new NotSupportedException();
+    }
+
+    public override void SetLength(long value)
+    {
+        throw new NotSupportedException();
+    }
+
+    public override void Write(byte[] buffer, int offset, int count)
+    {
+        throw new NotSupportedException();
+    }
+}
+
 public class FlowControlledStream : Stream
 {
     private ManualResetEvent _waitEvent = new ManualResetEvent(false);
