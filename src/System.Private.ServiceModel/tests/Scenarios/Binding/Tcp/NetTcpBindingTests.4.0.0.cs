cb5ae42cc32b3734068d75f7a4398f751ac0825a
Matt Connew <mconnew@users.noreply.github.com>
2023-05-03 13:10:15 -0700

    Fix disabling send/receive timers after send/receive operations completed. (#5138)

--- src/System.Private.ServiceModel/tests/Scenarios/Binding/Tcp/NetTcpBindingTests.4.0.0.cs
+++ src/System.Private.ServiceModel/tests/Scenarios/Binding/Tcp/NetTcpBindingTests.4.0.0.cs
@@ -2,7 +2,9 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using System.ServiceModel;
+using System.Threading;
 using System.Threading.Tasks;
 using Infrastructure.Common;
 using Xunit;
@@ -67,6 +69,43 @@ public partial class Binding_Tcp_NetTcpBindingTests : ConditionalWcfTest
             result = await serviceProxy.EchoAsync(testString).ConfigureAwait(false);
             Assert.Equal(testString, result);
             result = serviceProxy.Echo(testString);
+            Assert.Equal(testString, result);
+
+            // *** CLEANUP *** \\
+            ((ICommunicationObject)serviceProxy).Close();
+            factory.Close();
+        }
+        finally
+        {
+            // *** ENSURE CLEANUP *** \\
+            ScenarioTestHelpers.CloseCommunicationObjects((ICommunicationObject)serviceProxy, factory);
+        }
+    }
+
+    // Test for https://github.com/dotnet/wcf/issues/5134
+    [WcfFact]
+    [OuterLoop]
+    public static void ReceiveTimeout_Applied()
+    {
+        string testString = "Hello";
+        ChannelFactory<IWcfService> factory = null;
+        IWcfService serviceProxy = null;
+
+        try
+        {
+            // *** SETUP *** \\
+            NetTcpBinding binding = new NetTcpBinding(SecurityMode.None);
+            binding.OpenTimeout = TimeSpan.FromSeconds(10);
+            binding.SendTimeout = binding.ReceiveTimeout = TimeSpan.FromMinutes(10);
+            factory = new ChannelFactory<IWcfService>(binding, new EndpointAddress(Endpoints.Tcp_NoSecurity_Address));
+            serviceProxy = factory.CreateChannel();
+
+            // *** EXECUTE *** \\
+            ((IClientChannel)serviceProxy).Open();
+            Thread.Sleep(binding.OpenTimeout + TimeSpan.FromSeconds(5));
+            string result = serviceProxy.Echo(testString);
+
+            // *** VALIDATE *** \\
             Assert.Equal(testString, result);
 
             // *** CLEANUP *** \\
