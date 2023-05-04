c52449de05ffe07de7fde0cdc72ca71dee64d7e8
Carol Wang <v-carwan@microsoft.com>
2023-05-04 06:57:10 +0000

    Add NamedPipeMetadataImporter to dotnet-svcutil src.

---
+++ src/dotnet-svcutil/lib/NamedPipeMetadataImporter/IMetadataExchange.cs
@@ -0,0 +1,26 @@
+﻿using System.ServiceModel.Channels;
+using System.ServiceModel;
+
+namespace Microsoft.Tools.ServiceModel.Svcutil
+{
+    [ServiceContract]
+    public interface IMetadataExchange
+    {
+        [OperationContract(Action = WSTransfer.GetAction, ReplyAction = WSTransfer.GetResponseAction)]
+        Message Get(Message request);
+
+        [OperationContract(Action = WSTransfer.GetAction, ReplyAction = WSTransfer.GetResponseAction, AsyncPattern = true)]
+        IAsyncResult BeginGet(Message request, AsyncCallback callback, object state);
+        Message EndGet(IAsyncResult result);
+    }
+
+    public static class WSTransfer
+    {
+        public const string Prefix = "wxf";
+        public const string Name = "WS-Transfer";
+        public const string Namespace = "http://schemas.xmlsoap.org/ws/2004/09/transfer";
+
+        public const string GetAction = Namespace + "/Get";
+        public const string GetResponseAction = Namespace + "/GetResponse";
+    }
+}
---
+++ src/dotnet-svcutil/lib/NamedPipeMetadataImporter/NamedPipeMetadataImporter.cs
@@ -0,0 +1,76 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Xml;
+
+namespace Microsoft.Tools.ServiceModel.Svcutil
+{
+    public class NamedPipeMetadataImporter
+    {
+        const string UriSchemeNetPipe = "net.pipe";
+        const string NamedPipeBindingName = "MetadataExchangeNamedPipeBinding";
+        const string BindingNamespace = "http://schemas.microsoft.com/ws/2005/02/mex/bindings";
+        XmlReader? _xmlReader;
+
+        public XmlReader? GetReader(Uri uri)
+        {
+            return GetMetadatadataAsync(uri).GetAwaiter().GetResult();
+        }
+        public async Task<XmlReader?> GetMetadatadataAsync(Uri uri)
+        {
+            if(uri.Scheme != UriSchemeNetPipe)
+            {
+                return null;
+            }
+
+            ChannelFactory<IMetadataExchange> factory = new ChannelFactory<IMetadataExchange>(CreateNamedPipeBinding(), new EndpointAddress(uri.AbsoluteUri));
+            var proxy = factory.CreateChannel();
+            var messageVersion = factory.Endpoint.Binding.MessageVersion;
+
+            try
+            {
+                var _message = Message.CreateMessage(messageVersion, WSTransfer.GetAction);
+                IAsyncResult result = proxy.BeginGet(_message, new AsyncCallback(RequestCallback), proxy);
+
+                while (!result.IsCompleted)
+                {
+                    await Task.Delay(100);
+                }
+            }
+            catch
+            {
+                ((IClientChannel)proxy).Close();
+            }
+
+            finally
+            {
+                ((IClientChannel)proxy).Abort();
+            }
+
+            return _xmlReader;
+        }
+
+        public void RequestCallback(IAsyncResult result)
+        {
+            if (result.CompletedSynchronously)
+                return;
+
+            IMetadataExchange? metadataClient = result.AsyncState as IMetadataExchange;
+            if (metadataClient != null)
+            {
+                Message response = metadataClient.EndGet(result);
+                if (!response.IsFault)
+                {
+                    _xmlReader = response.GetReaderAtBodyContents();
+                }
+            }
+        }
+
+        public CustomBinding CreateNamedPipeBinding()
+        {
+            CustomBinding binding = new CustomBinding(NamedPipeBindingName, BindingNamespace);
+            NamedPipeTransportBindingElement pipeTransport = new NamedPipeTransportBindingElement();
+            binding.Elements.Add(pipeTransport);
+            return binding;
+        }
+    }
+}
