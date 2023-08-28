2814832f6ae288990f274553968ab536d3b2d185
Carol Wang <v-carwan@microsoft.com>
2023-08-28 07:26:10 +0000

    Add 1 more test case.

---
+++ src/dotnet-svcutil/lib/tests/TestCases/MultiTargetCloseAsyncGeneration/net48/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace net48
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
