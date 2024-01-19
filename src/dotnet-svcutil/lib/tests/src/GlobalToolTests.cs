ba13aab9ac627f225ab3dabc6f64f09b01ccd5e6
Carol Wang <v-carwan@microsoft.com>
2024-01-19 06:40:34 +0000

    rebase and fix test failure due to product upgrade.

--- src/dotnet-svcutil/lib/tests/src/GlobalToolTests.cs
+++ src/dotnet-svcutil/lib/tests/src/GlobalToolTests.cs
@@ -171,7 +171,7 @@ namespace SvcutilTest
         [Trait("Category", "BVT")]
         [Theory]
         [InlineData("-elm")]
-        public void ParamsFiles(string extraOptions)
+        public async void ParamsFiles(string extraOptions)
         {
             this_TestCaseName = "ParamsFiles";
             TestFixture();
