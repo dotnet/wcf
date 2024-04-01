6bd5a7fe5fa8729df885fab3bcdfc142de7f5701
Ahmed Afifi <v-aafifi@microsoft.com>
2024-04-01 06:14:31 +0200

    Add service ping

--- src/System.Private.ServiceModel/tools/SelfHostedCoreWcfService/Program.cs
+++ src/System.Private.ServiceModel/tools/SelfHostedCoreWcfService/Program.cs
@@ -15,11 +15,20 @@ namespace SelfHostedWCFService
             CertGenLib.SetupCerts();
 
             Console.WriteLine("Starting all service hosts...");
-            TestDefinitionHelper.StartHosts();
+            //TestDefinitionHelper.StartHosts()
+            var runHostTask = Task.Run(() => TestDefinitionHelper.StartHosts());
+            //runHostTask.Wait();
+            Task.Delay(5000).Wait();
 
             //Start the crlUrl service last as the client use it to ensure all services have been started
             TestHostServiceHost host = new TestHostServiceHost();
-            host.StartService();
+            //host.StartService();
+            var runTestHostTask = Task.Run(() => host.StartService());
+            //runTestHostTask.Wait();
+
+            // Ping to make sure the service is started
+            bool result = ServiceHostHelper.Ping("http://localhost:8081/TestHost.svc/Ping");
+            if (!result) { return; }
 
             Console.WriteLine("All service hosts have started.");
             do
