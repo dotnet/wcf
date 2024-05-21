ef3051cb9985e3b3c9b258bd4db044d8902f2f18
Carol Wang <v-carwan@microsoft.com>
2024-05-21 01:35:03 +0000

    dotnet-svcutil: add reference to common wcf feature packages only.

--- src/dotnet-svcutil/lib/src/Shared/TargetFrameworkHelper.cs
+++ src/dotnet-svcutil/lib/src/Shared/TargetFrameworkHelper.cs
@@ -101,11 +101,7 @@ namespace Microsoft.Tools.ServiceModel.Svcutil
             {new Version("8.0"), new List<ProjectDependency> {
                 ProjectDependency.FromPackage("System.ServiceModel.Http", "8.*"),
                 ProjectDependency.FromPackage("System.ServiceModel.NetTcp", "8.*"),
-                ProjectDependency.FromPackage("System.ServiceModel.NetNamedPipe", "8.*"),
-                ProjectDependency.FromPackage("System.ServiceModel.Primitives", "8.*"),
-                ProjectDependency.FromPackage("System.ServiceModel.Federation", "8.*"),
-                ProjectDependency.FromPackage("System.ServiceModel.UnixDomainSocket", "8.*"),
-                ProjectDependency.FromPackage("System.Web.Services.Description", "8.*")
+                ProjectDependency.FromPackage("System.ServiceModel.Primitives", "8.*")
             } }
         };
 
