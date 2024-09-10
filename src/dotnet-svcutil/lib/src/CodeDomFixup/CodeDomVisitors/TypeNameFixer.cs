f9f07b7525b10540c019ec63a71f1a1d7747c942
Carol Wang <v-carwan@microsoft.com>
2024-09-10 14:16:25 +0800

    dotnet-svcutil: add escaped prefix to types named with specific keywords (#5494)
    * dotnet-svcutil: add escaped prefix to types named with specific keywords
    
    * Address review feedback and update test.
    
    * Rebase and update file and class names.

---
+++ src/dotnet-svcutil/lib/src/CodeDomFixup/CodeDomVisitors/TypeNameFixer.cs
@@ -0,0 +1,44 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+using Microsoft.CodeDom;
+
+namespace Microsoft.Tools.ServiceModel.Svcutil
+{
+    internal class TypeNameFixup : CodeDomVisitor
+    {
+        //starting in c#11, types cannot be named as file/required/scoped, work around this by escaping with an @ prefix.
+        //ref: https://learn.microsoft.com/en-us/dotnet/csharp/whats-new/breaking-changes/compiler%20breaking%20changes%20-%20dotnet%207#types-cannot-be-named-file
+        private readonly List<string> _reservedKeyword = new List<string>() { "file", "required", "scoped" };
+        protected override void Visit(CodeCompileUnit cu)
+        {
+            foreach (CodeNamespace ns in cu.Namespaces)
+            {
+                foreach (CodeTypeDeclaration typeDeclaration in ns.Types)
+                {
+                    // Process the top-level type declaration and its nested types
+                    ProcessTypeAndMembers(typeDeclaration);
+                }
+            }
+        }
+
+        private void ProcessTypeAndMembers(CodeTypeDeclaration typeDeclaration)
+        {
+            if (_reservedKeyword.Contains(typeDeclaration.Name))
+            {
+                typeDeclaration.Name = "@" + typeDeclaration.Name;
+            }
+
+            foreach (CodeTypeMember member in typeDeclaration.Members)
+            {
+                if (member is CodeTypeDeclaration nestedType)
+                {
+                    // Recursively process the nested type
+                    ProcessTypeAndMembers(nestedType);
+                }
+            }
+        }
+    }
+}
