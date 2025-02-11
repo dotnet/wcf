cf0dd7f6ff1d5f7b89ec243ae6a6d89c18449d9e
Matt Connew <mconnew@users.noreply.github.com>
2025-02-11 14:56:01 -0800

    Fixes scenario where library references packages and NetFx app consumes library

---
+++ src/System.ServiceModel.NetTcp/ref/NetFxTypeForwards.cs
@@ -0,0 +1,15 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Runtime.CompilerServices;
+
+[assembly: TypeForwardedTo(typeof(System.ServiceModel.MessageSecurityOverTcp))]
+[assembly: TypeForwardedTo(typeof(System.ServiceModel.NetTcpBinding))]
+[assembly: TypeForwardedTo(typeof(System.ServiceModel.NetTcpSecurity))]
+[assembly: TypeForwardedTo(typeof(System.ServiceModel.TcpClientCredentialType))]
+[assembly: TypeForwardedTo(typeof(System.ServiceModel.TcpTransportSecurity))]
+[assembly: TypeForwardedTo(typeof(System.ServiceModel.Channels.TcpConnectionPoolSettings))]
+[assembly: TypeForwardedTo(typeof(System.ServiceModel.Channels.TcpTransportBindingElement))]
+[assembly: TypeForwardedTo(typeof(System.ServiceModel.Channels.ConnectionOrientedTransportBindingElement))]
+[assembly: TypeForwardedTo(typeof(System.ServiceModel.Channels.SslStreamSecurityBindingElement))]
+[assembly: TypeForwardedTo(typeof(System.ServiceModel.Channels.WindowsStreamSecurityBindingElement))]
